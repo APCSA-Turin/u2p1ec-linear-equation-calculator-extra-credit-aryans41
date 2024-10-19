8f04a2102e21b3376958b9c35454546663fb7e54
aryans41 <aryans41@nycstudents.net>
2024-10-19 22:46:18 +0000

    second commit

--- src/main/java/com/example/project/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.project;
-public class Main { 
-    public static void main(String[] args) {
-        LinearCalculator c = new LinearCalculator("(3,4)","(3,-4)");
-        String expectedOutput = "The midpoint of this line is: (3.0,0.0)";
-        String studentOutput = c.Midpoint();
-        System.out.println(studentOutput);
-    }
-}
